1aaa6160d01d17d305649e865f3f7a584dd07c1f
adeliasb <qa.asbs.lab@gmail.com>
2025-11-29 21:09:37 -0300

    fix: Corrigindo a estória de usuário

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,28 @@
 // cypress.config.js
-// Configuração do Cypress com integração do preprocessor para Cucumber
+// Configuração principal do Cypress com integração do preprocessor cucumber
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://lojaebac.ebaconline.art.br",
+    specPattern: "cypress/e2e/features/*.feature",
+    supportFile: "cypress/support/e2e.js",
+
+    setupNodeEvents(on, config) {
+      on("file:preprocessor", cucumber());
+      return config;
+    },
+  },
+
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+});
 
 module.exports = defineConfig({
   e2e: {
